7cfaaf35a3348edc850dc0254f323bdf1a168b73
Anton <antoniom@mail.ru>
2022-04-02 19:38:45 +0300

    02.04 - переделка MovieDB+Шаблон

--- js/script.js
+++ js/script.js
@@ -13,123 +13,120 @@
 5) Добавить нумерацию выведенных фильмов */
 
 'use strict';
-
-
-// Я сам))
-//Удалем рекламу
-// document.querySelector('.promo__adv').remove(); // простой вариант
-// или через цикл. удаляем именно изображения реклам но не блок
+window.addEventListener('load', ()=> {
+ 
 const adv = document.querySelectorAll('.promo__adv img');
-adv.forEach(item => { 
-  item.remove(); 
- });
-
-
-//Меняем жанр - можно через textContent также
-let genre  = document.querySelector('.promo__genre');
-genre.innerHTML = 'Драма';
-//Меняем фон постера
 const bg = document.querySelector('.promo__bg');
+let genre  = document.querySelector('.promo__genre');
+const formAdd = document.querySelector('form.add'); // сначала тег form + класс add
+const inputValue = document.querySelector('.adding__input');
+const btn = document.querySelector('button');
+const checkBox = document.querySelector('[data-checkBox]'); // или по типу ('[type="checkbox"]')
+const promoList = document.querySelector('.promo__interactive-list');
+
+//Удаляем рекламу
+const deleteAdv = (removeElement)=> {
+  removeElement.forEach(item => { 
+    item.remove(); 
+  });
+};
+
+deleteAdv(adv);
+
+//Меняем жанр и фон - можно через textContent также
+const changeSome = ()=> {
+  genre.innerHTML = 'Драма';
+  //Меняем фон постера
 bg.style.background = 'url(../img/bg.jpg)';
-bg.style.maxWidth  = '100%';
+
+};
+changeSome();
+
+
 
 
 // формируем базу фильмов из JS и сортируем
 const movieDB = {
   movies: [
     "Одержимость",
-      "Логан2",
+       "Логан2",
       "Лига справедливости",
       "Ла-ла лэнд-1",
       "Скотт Пилигрим против...",
-      
-  ]
+    ]
 };
-// movieDB.movies.sort();
- const promoList = document.querySelector('.promo__interactive-list');
- 
- 
- movieDB.movies.forEach((item, i) => { 
-   promoList.innerHTML += `<li class="promo__interactive-item"> ${i + 1 } ${item}<div class="delete"></div>
-   </li>`;
-   // console.log(item);
-  });
 
+// 1 - В HTML пишем теги с ыильмами на то случай если у кого то будет отключен JS чтобы шаблон с фил. бфл виден.
+// 2. Запускаем ф.createMovieList - она очищает HTML и добавляет из массива наши ф. + сортирует их. Туда же надо добавить Обрабьотчик удаления фильмов .т.к. если снаружи фун. то не будет удалять новые фил.
+// 3. Создаем Обработчки Клик для ввода новых фильмов - 
+
+// Сортируем фильмы в массиве сразу 
+const sortArr = (arr) => {
+  arr.sort();
+};
 
 
 // Добавляем фильмы из формы ввода в массив с фильмами
-const inputValue = document.querySelector('.adding__input');
-const btn = document.querySelector('button');
-const checkBox = document.querySelector('[data-checkBox]');
+// Добавляем независмимые параметры в ф. чтобы можно было любой элемент добавлять  и только в аргументах при вызове ставить нужные элементы из DOM 13-23 №33
+function createMovieList(films, parent) { 
+  parent.innerHTML = '';  // очищаем HTML 
+  sortArr(films);
 
-
-// let i = 0;
-btn.addEventListener('click', (e)=> {
-  e.preventDefault(); // отключаем перезагрузку страницы
-  let inputLength = inputValue.value.length;
-  let inputText = inputValue.value;
-  if(inputLength > 5) {
-   }
-   // этот прием удаляет и заменяет предыдущие значения, не подойдет
-  // movieDB.movies[i] = inputText.slice(0, 5) + '\u2026';
-// push вставляет сразу в конец, а надо вставлять сразу в нужное место по алфавиту
-
- movieDB.movies.push(inputText.slice(0, 5) + '\u2026');
-  // i++;
-  movieDB.movies.sort();
-  console.log(movieDB);
-  promoList.innerHTML = '';  // очищаем после каждого ввода все фильмы и заново прогоняем цикл и берем данные из массива
-  
-  movieDB.movies.forEach((item, i) => { 
-    promoList.innerHTML += `<li class="promo__interactive-item"> ${i + 1 } ${item}<div class="delete"></div>
+  films.forEach((item, i) => { 
+    parent.innerHTML += `<li class="promo__interactive-item"> ${i + 1 } ${item}<div class="delete"></div>
     </li>`;
-    // console.log(item);
-   
    });
+     // Удаляем фильмы из базы по клику на корзине / Важно поместить внутрь Функции createMovieList иначе новые ф. не удаляет
  
+  document.querySelectorAll('.delete').forEach((item, i) => {
+    item.addEventListener('click', (e)=> { 
+     item.parentNode.remove(); // удалем весь родительский узел у корзины или  item.parentElement
    
-
-});
-
-// movieDB.movies.sort();
-// const promoList = document.querySelector('.promo__interactive-list');
-// promoList.innerHTML = '';
-// movieDB.movies.forEach((item, i) => { 
-//   promoList.innerHTML += `<li class="promo__interactive-item"> ${i + 1 } ${item}<div class="delete"></div>
-//   </li>`;
-//   // console.log(item);
-//  });
-
-// const promoList = document.querySelectorAll('.promo__interactive-item');
-// promoList.forEach((item, i) => { 
-//   //Сначала удаляем содержимое из HTML
-//   item.innerHTML =  '';
-//   // item.innerHTML = `${i + 1 } `+movieDB.movies[i];
-//   // Но лучше так, так как позже надо будет класс delete использовать
-//   item.innerHTML = `<li class="promo__interactive-item"> ${i + 1 } ${movieDB.movies[i]}<div class="delete"></div>
-// </li>`;
-//   //или так
-//   //  item.insertAdjacentHTML('afterbegin', `${i+1} `); 
-//  });
-
-
-//  console.log(movieDB.movies);
-
-// Удаляем фильмы из базы по клику на корзине
-const trash = document.querySelectorAll('.delete');
-trash.forEach((item, i) => {
-    item.addEventListener('click', ()=> { 
-      trash[i].parentNode.remove(); // удалем весь родительский узел у корзины
+      movieDB.movies.splice(i, 1);
+      createMovieList(films,parent); // рекурсия - еще раз вызываем себя - ф. чтобы заново отсортировала массив ( как бы освежает сама себя)
+      console.log(movieDB.movies);
+      
     });
   
  });
+}
+createMovieList(movieDB.movies,promoList);
 
-   //Проверяем чекбокс на галочку
-   checkBox.addEventListener('change', ()=> { 
-    if(checkBox.checked) {
-      console.log('Да');
+// Добавляем фильмы в массив и HTML по клику на кнопке -Добавить
+ formAdd.addEventListener('submit', (event)=> { // но можно click 
+  event.preventDefault(); // отключаем перезагрузку страницы и отправку формы
+  let newFilm = inputValue.value;
+  let favorite = checkBox.checked; // true
+  if(newFilm) { // если не пустая строка ввода
+    if(newFilm.length > 3) {
+      newFilm = newFilm.slice(0, 3) + '\u2026'; // или substring
+      // newFilm =`${newFilm.slice(0, 5)}...` ;
+     } 
+        //Проверяем чекбокс на галочку
+    if(favorite) {
+      console.log('Любимый фильм Мой!');
     } else {
       console.log('Нет');
     }
     
-  });
+    movieDB.movies.push(newFilm);
+    sortArr(movieDB.movies);
+   createMovieList(movieDB.movies,promoList);
+
+   //  inputValue.value = ''; // или просто input очистить после клика
+   //или
+// event.target.reset();
+   formAdd.reset(); // или обратиться к самой форме
+    console.log(movieDB);
+
+  }
+}); 
+ 
+
+
+
+
+
+
+}); //конец load
+
